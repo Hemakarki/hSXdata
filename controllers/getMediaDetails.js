fb35c5e59e7c57c57b19de747a52c72a91c9e117
Hema karki <hemakarki111.hk@gmail.com>
2017-12-13 19:53:19 +0530

    media

--- controllers/getMediaDetails.js
+++ controllers/getMediaDetails.js
@@ -1,6 +1,10 @@
 let ig = require('instagram-node').instagram();
 
 exports.getMediaDetails = function(req, res, next) { 
+    let most_liked_data = [];
+    let liked_media = [];
+    let most_commented_data = [];
+    let commented_media = [];
     if (!req.body.access_token) {
         return  res.status(400).send({
           'status': 'error',
@@ -11,7 +15,8 @@ exports.getMediaDetails = function(req, res, next) {
         let access_token = req.body.access_token;
         ig.use({ access_token: access_token });
         let userId = req.body.access_token.split('.')[0];
-        
+        let hemadata = getFollowers(userId);
+
         // get list of users who liked recent upload
         ig.user_media_recent(userId, [], function(err, medias, pagination, remaining, limit) {
             if(err) {
@@ -26,68 +31,77 @@ exports.getMediaDetails = function(req, res, next) {
                             'message': 'no data available.'
                           });
                     }else{
+                        let Count = countMax(medias);
                         for(let i=0; i<medias.length; i++){
-                            console.log(medias[i].user_has_liked,"tadaaa")
+                            if(medias[i].likes.count == Count.likecount){
+                                most_liked_data.push(medias[i])
+                            }
+                            if(medias[i].comments.count == Count.commentcount){
+                                most_commented_data.push(medias[i])
+                            }
                         }
-                    }
-                    let dummyResult = {
-                            "data": [{
-                                "username": "jiyakarki",
-                                "first_name": "Jiya",
-                                "last_name": "karki",
-                                "type": "user",
-                                "id": "660033"
-                            },
-                            {
-                                "username": "sammyjack",
-                                "first_name": "Sammy",
-                                "last_name": "Jack",
-                                "type": "user",
-                                "id": "29648"
-                            }]
+                        for(i=0; i<most_liked_data.length; i++){
+                            most_liked_media = {
+                            likes : most_liked_data[i].likes.count,
+                            url : most_liked_data[i].images.standard_resolution
+                            }
+                            liked_media[i]= most_liked_media;
+                        }
+                        for(i=0; i<most_commented_data.length; i++){
+                            most_commented_media = {
+                            comments : most_commented_data[i].comments.count,
+                            url : most_commented_data[i].images.standard_resolution
+                            }
+                            commented_media[i]= most_commented_media;
+                        }
+                        // to count maximum count of like and
+                        function countMax(medias){
+                            var highestValuelike = 0;
+                            var highestValuecomment = 0;
+                            for (var i=0, len = medias.length; i<len; i++) {
+                              var likeValue = Number(medias[i].likes.count);
+                              var commentValue = Number(medias[i].comments.count);
+                              if (likeValue > highestValuelike) {
+                                highestValuelike = likeValue;
+                              }
+                              if (commentValue > highestValuecomment) {
+                                highestValuecomment = commentValue;
+                            }
+                            } return highestValue={
+                                "likecount" : highestValuelike,
+                                "commentcount" : highestValuecomment
+                            };
                         }
                         return res.status(200).send({
                             'status': 'success',
                             'messageId': 200,
                             'message': 'successfully accessed.',
-                            'user' : dummyResult
+                            'most_liked_media' : liked_media,
+                            'most_commented_media' : commented_media
                         });
-                    console.log(dummyResult,"here you go!")
+                    }
                 });
             }
         });
-        // ig.user_follows(userId, function(err, users, pagination, remaining, limit) {
-        //     if(err) {
-        //         console.log(err);
-        //         return res.status(400).send({
-        //             'status': 'failed',
-        //             'messageId': 400,
-        //             'message': 'error occured.',
-        //             'error' : err
-        //           });
-        //     }else{
-        //         let dummyUser = {
-        //                 "data": [{
-        //                     "username": "kevin",
-        //                     "profile_picture": "http://images.ak.instagram.com/profiles/profile_3_75sq_1325536697.jpg",
-        //                     "full_name": "Kevin Systrom",
-        //                     "id": "3"
-        //                 },
-        //                 {
-        //                     "username": "instagram",
-        //                     "profile_picture": "http://images.ak.instagram.com/profiles/profile_25025320_75sq_1340929272.jpg",
-        //                     "full_name": "Instagram",
-        //                     "id": "25025320"
-        //                 }]
-        //             }
-        //         return res.status(200).send({
-        //             'status': 'success',
-        //             'messageId': 200,
-        //             'message': 'successfully accessed.',
-        //             'user' : dummyUser
-        //         });
-        //     }
-        // });
+      
+        // get the follower list of the user
+        function getFollowers(userId){
+            let data=[] ;
+            ig.user_follows(userId, function(err, users, pagination, remaining, limit) {
+                if(err) {
+                    console.log(err);
+                    return err ;
+                }else{
+                    data= users;
+                }
+                return usersdata={
+                    'user' : data
+                };
+            });
+            return usersdata={
+                'user' : data
+            };
+        }
     }
     console.log("hema ;)")
 }
